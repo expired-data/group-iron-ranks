39646c03c69c8dcb590937454f3bcf132471f2d9
A Ridley <expireddata@gmail.com>
2022-08-02 13:21:40 +0100

    feat: add ranks

--- src/components/Score.tsx
+++ src/components/Score.tsx
@@ -2,8 +2,43 @@ interface ScoreProps {
   score: number;
 }
 
+const Ranks: {name: string, points: number}[] = [
+    { 
+        name: "Noob", 
+        points: 0
+    }, 
+    { 
+        name: "Goon", 
+        points:  2
+    },
+    { 
+        name: "Knight", 
+        points: 6
+    }, 
+    { 
+        name: "Paladin", 
+        points: 14
+    },
+    { 
+        name: "Legend", 
+        points: 20
+    }, 
+    { 
+        name: "Myth", 
+        points: 28
+    },
+    { 
+        name: "Crusader", 
+        points: 33
+    }, 
+    { 
+        name: "Beast", 
+        points: 38
+    }
+]
+
 const Score = ({ score }: ScoreProps): JSX.Element => {
-  return <div>Your score is {score}</div>;
+  return <div>Your score is {score}. This gives you the rank of {Ranks.filter(({points}) => points <= score).at(-1)?.name} </div>;
 };
 
 export { Score };
